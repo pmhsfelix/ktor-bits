404699955a02e0fff0d28a8eb0015506d89e1075
Pedro Felix <pmhsfelix@gmail.com>
2019-04-13 19:31:55 +0100

    first: accessing the request and the response

--- first/src/main/kotlin/first/TheAppModule.kt
+++ first/src/main/kotlin/first/TheAppModule.kt
@@ -5,6 +5,10 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
+import io.ktor.request.ApplicationRequest
+import io.ktor.request.httpMethod
+import io.ktor.request.uri
+import io.ktor.response.ApplicationResponse
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
@@ -13,7 +17,12 @@ fun Application.module() {
     install(DefaultHeaders)
     install(Routing) {
         get("/") {
-            call.respondText("Hello World from ktor, using modules", ContentType.Text.Plain)
+            val request : ApplicationRequest = call.request
+            val method = request.httpMethod
+            val uri = request.uri // will not be the absolute URI
+            val response : ApplicationResponse = call.response
+            response.headers.append("My-Header", "My-Value")
+            call.respondText("Request with method=${method.value}, URI=$uri", ContentType.Text.Plain)
         }
     }
 }

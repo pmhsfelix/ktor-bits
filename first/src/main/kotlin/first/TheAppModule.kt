350d01707fe7234efc23e00bce40b37d58e42ce8
Pedro Felix <pmhsfelix@gmail.com>
2019-04-13 19:20:20 +0100

    first: adds a feature

--- first/src/main/kotlin/first/TheAppModule.kt
+++ first/src/main/kotlin/first/TheAppModule.kt
@@ -3,12 +3,14 @@ package first
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 
 fun Application.module() {
+    install(DefaultHeaders)
     install(Routing) {
         get("/") {
             call.respondText("Hello World from ktor, using modules", ContentType.Text.Plain)
